4a0126cc9917ebba1b7ffb8fbfe08ff5095d809e
Kendra Seaman <kendra@Kendras-MacBook-Pro.local>
2019-01-30 22:19:40 -0500

    Update Table 2

--- scr/ageBPdescriptive.R
+++ scr/ageBPdescriptive.R
@@ -29,11 +29,11 @@ rm(d2,d3,d4,d5,t)
 # Study Means
 d2 <- d1[which(d1$Study == 'SV'),]
 u <- as.data.frame(cbind(colnames(d2)[5:ncol(d2)], paste0(round(colMeans(d2[5:ncol(d2)], na.rm = TRUE),2), 
-                                                          ' ( ', round(apply(d2[5:ncol(d2)],2,sd, na.rm = TRUE), 2), ' )')))
+                                                          ' (', round(apply(d2[5:ncol(d2)],2,sd, na.rm = TRUE), 2), ')')))
 colnames(u) <- c('Region', 'Study 1 BPND M (SD)')
 d3 <- d1[which(d1$Study == 'DND'),]
 v <- as.data.frame(cbind(colnames(d3)[5:ncol(d3)], paste0(round(colMeans(d3[5:ncol(d3)], na.rm = TRUE),2), 
-                                                          ' ( ', round(apply(d3[5:ncol(d3)],2,sd, na.rm = TRUE), 2), ' )')))
+                                                          ' (', round(apply(d3[5:ncol(d3)],2,sd, na.rm = TRUE), 2), ')')))
 colnames(v) <- c('Region', 'Study 2 BPND M (SD)')
 d4 <- merge(u,v, by = 'Region')
 d5 <- merge(d6,d4, by ='Region')
@@ -43,25 +43,16 @@ rm(d1,d2,d3,d4,d6,u,v)
 s <- matrix(nrow = 0, ncol=2)
 for (r in ROI){
   a <- t.test(dt$BPnd[dt$ROI == r] ~ dt$Study[dt$ROI == r])
-  row <- cbind(r, paste0(round(a$statistic,2), ' [ ', round(a$conf.int[1],2), ' , ', round(a$conf.int[2],2), ' ]'))
+  row <- cbind(r, paste0(round(a$statistic,2), ' [', round(a$conf.int[1],2), ', ', round(a$conf.int[2],2), ']'))
   s <- rbind(s,row)
 }
 d7 <- merge(d5[c(1, 4, 2:3, 6:7)],s, by.x = 'Region' , by.y = 'r' )
-
-# clean up names 
-d7$name <- as.character(d7$name)
-d7$name[which(d7$Region == 'Ant_TL_med_bil')] <- 'Anterior temporal lobe, medial'
-d7$name[which(d7$Region == 'Ant_TL_inf_lat_bil')] <- 'Anterior temporal lobe, lateral'
-d7$name[which(d7$Region == 'G_sup_temp_post_bil')] <- 'Superior temporal gyrus, posterior'
-d7$name[which(d7$Region == 'G_sup_temp_ant_bil')] <- 'Superior temporal gyrus, anterior'
-d7$name[which(d7$Region == 'G_cing_ant_bil')] <- 'Cingulate gyrus, anterior'
-d7$name[which(d7$Region == 'G_cing_post_bil')] <- 'Cingulate gyrus, posterior'
-d7$name <- as.factor(d7$name)
 
 # reorder by average BPND
 d7$`avg BPND` <- as.numeric(as.character(d7$`avg BPND`))
 d8 <- d7[order(-d7$`avg BPND`),]
 d9 <- d8[c(2:3,5:7)]
+rm(a,r,s,row,d5,d6,d6,d7,d8,ROI)
 
 colnames(d9) <- c('Region', 'Overall BPND M (SD)', 'Study 1 BPND M (SD)', 'Study 2 BPND M (SD)', 'Study t-value [95% CI]')
 
